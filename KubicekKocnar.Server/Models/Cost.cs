ee22c5e75de3bfc5f1141dc8bf2fec544561900d
Tomáš <tomas.kubicek.022@pslib.cz>
2024-12-13 16:29:34 +0100

    migrace a outfit

--- KubicekKocnar.Server/Models/Cost.cs
+++ KubicekKocnar.Server/Models/Cost.cs
@@ -1,5 +1,6 @@
 ﻿namespace KubicekKocnar.Server.Models {
     public class Cost {
+        public uint CostId { get; set; }
         public uint Count { get; set; }
         public Coinage? Coinage { get; set; }
         public uint CoinageId { get; set; }
