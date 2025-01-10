caf23048851fa52780f5cb7c270e9e946807e4a3
tkdeeev <tomas.kubicek.022@pslib.cz>
2025-01-10 09:57:12 +0100

    mainmenu nz

--- kubicekkocnar.client/src/app/+index.tsx
+++ kubicekkocnar.client/src/app/+index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/index.css"
 
 const MainMenu: React.FC = () => {
     return (
         <div className='mainmenu'>
-            <h1>Main Menu</h1>
-            <div>
-                <Link className='button' to="/games/1/levels/1">
-                Play
-                </Link>
+            <div className="mainmenu__headings">
+                <h3 className='mainmenu__subheading'>fr rpg</h3>
+                <h1 className='mainmenu__heading'>corrupted</h1>
             </div>
-            <div>
+            <div className="mainmenu__buttons">
+                <Link className='button' to="/games/1/levels/1">
+                    Play
+                </Link>
+                <Link className='button mainmenu__semi' to="/games/1/levels/1">
+                    Continue
+                </Link>
                 <Link className='button' to="/games">
                     Browse Games
                 </Link>
+            </div>
+            <div className="mainmenu__footer mainmenu__semi">
+                <p>TOMAS KUBICEK</p>
+                <p>JAN KOCNAR</p>
             </div>
         </div>
     );
