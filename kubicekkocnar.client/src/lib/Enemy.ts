a3d556d90b2540d0c3b13ab1a8156823cb5f620c
janek-123 <jan.kocnar.022@pslib.cz>
2025-01-15 11:25:34 +0100

    Pathfinding quickfix

--- kubicekkocnar.client/src/lib/Enemy.ts
+++ kubicekkocnar.client/src/lib/Enemy.ts
@@ -60,15 +60,9 @@ export class EnemyRenderer {
         {
           //enemy.mesh.position.add(player.position.clone().sub(enemy.mesh.position).normalize().multiplyScalar(enemy.speed / 1000));
 
-          const blockBelowEnemy = this.level.blocks.find(block => 
-            block.position.x === Math.round(enemy.mesh.position.x) &&
-            block.position.z === Math.round(enemy.mesh.position.z)
-          );
+          const blockBelowEnemy = this.pathFinder.GetClosestBlock(enemy.mesh.position.clone().sub(new THREE.Vector3(0, 2, 0)));
 
-          const blockBelowPlayer = this.level.blocks.find(block =>
-            block.position.x === Math.round(player.position.x) &&
-            block.position.z === Math.round(player.position.z)
-          );
+          const blockBelowPlayer = this.pathFinder.GetClosestBlock(player.position.clone().sub(new THREE.Vector3(0, 2, 0)));
 
           const path = this.pathFinder.FindPath(blockBelowEnemy, blockBelowPlayer);
 
@@ -85,8 +79,9 @@ export class EnemyRenderer {
           
           blockBelowEnemy!.mesh.material = new THREE.MeshStandardMaterial({color: 0xff0000});
           blockBelowPlayer!.mesh.material = new THREE.MeshStandardMaterial({color: 0xff0000});
+          
 
-          if (path.length > 0)
+          if (path.length > 1)
           {
             const nextBlock = path[1];
             enemy.mesh.position.add(nextBlock.position.clone().add(new THREE.Vector3(0, 2, 0)).sub(enemy.mesh.position).normalize().multiplyScalar(enemy.speed / 1000));
