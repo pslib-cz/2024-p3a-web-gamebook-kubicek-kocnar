a46b1b708b6a36c7f8126bc530de4262ed645d19
janek-123 <jan.kocnar.022@pslib.cz>
2025-02-07 08:58:01 +0100

    Enemies migration

---
+++ KubicekKocnar.Server/Migrations/20250207074422_Enemies.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KubicekKocnar.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Enemies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Enemy");
+
+            migrationBuilder.CreateTable(
+                name: "Enemy",
+                columns: table => new
+                {
+                    EnemyId = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Health = table.Column<float>(type: "REAL", nullable: false),
+                    Damage = table.Column<float>(type: "REAL", nullable: false),
+                    AttackSpeed = table.Column<float>(type: "REAL", nullable: false),
+                    Speed = table.Column<float>(type: "REAL", nullable: false),
+                    IsGhost = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TextureId = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enemy", x => x.EnemyId);
+                    table.ForeignKey(
+                        name: "FK_Enemy_Textures_TextureId",
+                        column: x => x.TextureId,
+                        principalTable: "Textures",
+                        principalColumn: "TextureId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cost");
+
+            migrationBuilder.DropTable(
+                name: "EnemyLevel");
+
+            migrationBuilder.DropTable(
+                name: "Features");
+
+            migrationBuilder.DropTable(
+                name: "PlacedBlocks");
+
+            migrationBuilder.DropTable(
+                name: "RoleUser");
+
+            migrationBuilder.DropTable(
+                name: "UserClaims");
+
+            migrationBuilder.DropTable(
+                name: "Coinages");
+
+            migrationBuilder.DropTable(
+                name: "ItemUpgrades");
+
+            migrationBuilder.DropTable(
+                name: "PlayerUpgrades");
+
+            migrationBuilder.DropTable(
+                name: "Enemy");
+
+            migrationBuilder.DropTable(
+                name: "Blocks");
+
+            migrationBuilder.DropTable(
+                name: "Levels");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "Textures");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+        }
+    }
+}
