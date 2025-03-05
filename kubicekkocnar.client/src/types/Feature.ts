eba6dda4c686d8eb68197e64f73c8f544b8c39c7
Tomáš <tomas.kubicek.022@pslib.cz>
2025-03-05 20:39:11 +0100

    and songs keep getting louder

--- kubicekkocnar.client/src/types/Feature.ts
+++ kubicekkocnar.client/src/types/Feature.ts
@@ -28,6 +28,8 @@ enum FeatureTypeIcon {
   "orders" = 2,
   "call_split" = 3,
   "diamond" = 4,
+  "menu_book" = 5,
+    "fastfood" = 6,
 }
 
 // the FeatureParams type is used to define the properties of a feature -> its content depends on the FeatureType
